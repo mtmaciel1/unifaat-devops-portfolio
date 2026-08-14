3b9b5cd9b9e6013cd163051d69a16c8ea11b9db4
mtmaciel <mtmaciel7@gmail.com>
2026-08-14 13:09:25 -0300

    feat: atualiza mensagem para v2

--- aula-01/app/server.js
+++ aula-01/app/server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({
-    servico: 'DevOps Portfolio API',
+    servico: 'DevOps Portfolio API v2',
     aluno: 'SEU NOME AQUI',
     ra: 'SEU RA AQUI',
     aula: '01 - Fundamentos de Git e Docker',
